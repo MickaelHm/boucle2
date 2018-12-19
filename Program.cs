6cf89c8e093cb9c0e9573b09e0a59f8782e749e0
MickaelHm <harmeau.mickael@gmail.com>
2018-12-19 11:30:04 +0100

    complet

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boucle2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop = 0;
+            int number = 1;
+            while (loop < 10) // temps que la boucle "loop" est inférieur a 10 répétion, elle continue
+            {
+                Console.WriteLine("Bonjour, je suis le message n°" + number++);
+                loop++;
+            }
+        }
+    }
+}
